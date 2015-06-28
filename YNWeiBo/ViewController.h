20288ef7f2278fcfc4229af79c8303001002d9f8
james_cai <csi_1314@126.com>
2015-06-28 16:56:41 +0800

    init project

---
+++ YNWeiBo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YNWeiBo
+//
+//  Created by james on 15/6/28.
+//  Copyright (c) 2015年 kfvnx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
