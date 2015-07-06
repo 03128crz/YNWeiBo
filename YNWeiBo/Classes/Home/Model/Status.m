baa7e7267fa5fd7f441b983264df73d34fca8831
james_cai <csi_1314@126.com>
2015-07-06 22:53:51 +0800

    + 发微博时间 处理

--- YNWeiBo/Classes/Home/Model/Status.m
+++ YNWeiBo/Classes/Home/Model/Status.m
@@ -16,4 +16,91 @@
 }
 
 
+-(NSString *)created_at{
+    
+    NSDateFormatter *fmt = [[NSDateFormatter alloc] init];
+    fmt.dateFormat = @"EEE MMM dd HH:mm:ss Z yyyy";
+    //真机调试，转换这种欧美时间，需要设置locale
+    fmt.locale = [[NSLocale alloc] initWithLocaleIdentifier:@"en_US"];
+    //微博的创建日期
+    NSDate *createDate = [fmt dateFromString:_created_at];
+    //当前时间
+    NSDate *now = [NSDate date];
+    // 日历对象，方便比较两个日期之间的差距
+    NSCalendar *calendar = [NSCalendar currentCalendar];
+   //想要获取的差值
+    NSCalendarUnit unit = NSCalendarUnitYear | NSCalendarUnitMonth | NSCalendarUnitDay|
+    NSCalendarUnitHour | NSCalendarUnitMinute | NSCalendarUnitSecond;
+    
+    NSDateComponents *components = [calendar components:unit fromDate:createDate toDate:now options:0];
+    
+    if ( [self isThisYear:createDate ]) {
+        if ([self isYesterday:createDate]) {
+            fmt.dateFormat = @"昨天 HH:mm";
+            return [fmt stringFromDate:createDate];
+        } else if([self isToday:createDate]){
+            if (components.hour>=1) {
+                return [NSString stringWithFormat:@"%ld小时前",(long)components.hour];
+            }else if (components.minute>=1){
+                return [NSString stringWithFormat:@"%ld分钟前",(long)components.minute];
+            }else{
+                return @"刚刚";
+            }
+        }else{
+            fmt.dateFormat = @"MM-dd HH:mm";
+            return  [ fmt stringFromDate:createDate];
+        }
+    } else {
+        fmt.dateFormat = @"yyyy-MM-dd HH:mm";
+        return [fmt stringFromDate:createDate];
+    }
+    
+    //fmt.dateFormat = @"yyyy-MM-dd HH:mm:ss";
+    
+}
+
+-(BOOL)isThisYear:(NSDate *)date{
+    
+    NSCalendar *calendar = [NSCalendar currentCalendar];
+    
+    NSDateComponents *dateCmps = [calendar components:NSCalendarUnitYear fromDate:date];
+    
+    NSDateComponents *nowCmps = [calendar components:NSCalendarUnitYear fromDate:[NSDate date]];
+    
+    return dateCmps.year ==nowCmps.year;
+}
+
+-(BOOL)isYesterday:(NSDate *)date{
+    NSDateFormatter *fmt = [[NSDateFormatter alloc]init];
+    fmt.dateFormat = @"yyyy-MM-dd";
+    
+    NSString *dateStr = [fmt stringFromDate:date];
+    
+    NSString *nowStr = [fmt stringFromDate:[NSDate date]];
+    
+    date = [fmt dateFromString:dateStr];
+    
+    NSDate *now = [fmt dateFromString:nowStr];
+    
+    NSCalendar *calendar = [NSCalendar currentCalendar];
+    
+    NSCalendarUnit unit = NSCalendarUnitYear | NSCalendarUnitMonth | NSCalendarUnitDay;
+    
+    NSDateComponents *cmps = [calendar components:unit fromDate:date toDate:now options:0];
+    
+    return cmps.day==0 && cmps.month ==0 && cmps.day==1;
+}
+
+-(BOOL)isToday:(NSDate *)date{
+    
+    NSDate *now = [NSDate date];
+    NSDateFormatter *fmt = [[NSDateFormatter alloc]init];
+    fmt.dateFormat = @"yyyy-MM-dd";
+    
+    NSString *dateStr = [fmt stringFromDate:date];
+    NSString *nowStr = [fmt stringFromDate:now];
+    
+    return [dateStr isEqualToString:nowStr];
+}
+
 @end
