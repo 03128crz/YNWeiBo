61bffec4516ac981501b8db8cfb073a235108623
james_cai <csi_1314@126.com>
2015-07-10 00:05:19 +0800

    添加扩展

---
+++ YNWeiBo/Classes/Main/Extension/NSDate+Extension.h
@@ -0,0 +1,19 @@
+//
+//  NSDate+Extension.h
+//  YNWeiBo
+//
+//  Created by james on 15/7/6.
+//  Copyright (c) 2015年 kfvnx. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSDate (Extension)
+
+-(BOOL)isThisYear;
+
+-(BOOL)isYesterday;
+
+-(BOOL)isToday;
+
+@end
---
+++ YNWeiBo/Classes/Main/Extension/UIWindow+Extension.h
@@ -0,0 +1,15 @@
+//
+//  UIWindow+Extension.h
+//  YNWeiBo
+//
+//  Created by james on 15/7/4.
+//  Copyright (c) 2015年 kfvnx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIWindow (Extension)
+
+-(void)switchRootViewController;
+
+@end
